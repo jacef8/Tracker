9f416236e0c2e3677783e7d4b0ede97c5f3158d6
jacef8 <jaceford08@gmail.com>
2026-06-13 23:06:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-//test
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/version', function(req, res) {
+  res.setHeader('Cache-Control', 'no-store');
+  res.send('1.6');
+});
+
+app.get('/777', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, function() {
+  console.log('GroundLink on port ' + PORT);
+});
